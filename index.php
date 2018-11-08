9c16a7e747867caac5bb33903997ab56ccab1901
test <gichira44@gmail.com>
2018-11-08 02:16:14 -0800

    use constant

--- index.php
+++ index.php
@@ -19,11 +19,21 @@
 			$age=20;
 
 			echo "$name is my name and am $age yrs old<br/>";
-			echo $name. "is my name and am" .$age. "yrs old";
+			echo $name. "is my name and am" .$age. "yrs old<br/>";
 			
 
 
 			?>
+
+			<?php
+
+			define('dirpath', 'lang/css');
+			echo dirpath;
+
+			?>
+
+
+
 		</body>
 	</head>
 </html>
