edd5d51dd59d118aa09e21601a03d1fed8142699
test <gichira44@gmail.com>
2018-11-08 02:04:06 -0800

    intial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+		<body>
+			<?php
+
+			echo"Hellow world <br/>";
+			print ("<b>Hellow world</b>");
+
+
+			?>
+
+			<br/>
+
+			<?php
+
+			echo"<i>Hellow<i> world <br/>";
+
+			$name="jacob";
+			$age=20;
+
+			echo "$name is my name and am $age yrs old<br/>";
+			echo $name. "is my name and am" .$age. "yrs old";
+			
+
+
+			?>
+		</body>
+	</head>
+</html>
